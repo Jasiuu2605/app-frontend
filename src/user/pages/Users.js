f0543f9ec9ed18b2ed8d6967cfb28d8f25541780
Jan Bańczerowski <jasiuu2605@gmail.com>
2024-02-27 19:21:34 +0100

    adding connection from user page to user item

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList/UsersList";
+
 const Users = () => {
-  return <h2>Users Works</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Jan Bańczerowski",
+      image:
+        "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
