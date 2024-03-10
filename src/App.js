f442d590a6fa0185f611743c19e10c280c5b6630
Jan Bańczerowski <jasiuu2605@gmail.com>
2024-03-10 15:56:08 +0100

    adding sideDrawer component

--- src/App.js
+++ src/App.js
@@ -8,21 +8,25 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
 
-        <Route path="/places/new">
-          <NewPlace />
-        </Route>
+          <Route path="/places/new">
+            <NewPlace />
+          </Route>
 
-        <Redirect to="/" />
-      </Switch>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
