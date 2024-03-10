e8027362f04b8c11928cde0085a9780f53a77f6a
Jan Bańczerowski <jasiuu2605@gmail.com>
2024-03-10 16:17:24 +0100

    adding fuctionality of closing sidedrawer after clicking on link

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -4,9 +4,13 @@ import ReactDOM from "react-dom";
 import "./SideDrawer.css";
 
 const SideDrawer = (props) => {
-  const content = <aside className="side-drawer">{props.children}</aside>;
+  const content = (
+    <aside onClick={props.onclick} className="side-drawer">
+      {props.children}
+    </aside>
+  );
 
-  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
+  return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
 };
 
 export default SideDrawer;
