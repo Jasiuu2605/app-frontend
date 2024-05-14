52825c64ffed81e169fb7f6930cc68acc687c608
Jan Bańczerowski <jasiuu2605@gmail.com>
2024-05-14 20:57:35 +0200

    Adding new place input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -2,11 +2,18 @@ import React from "react";
 import "./NewPlace.css";
 
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" element="input" />
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter valid title"
+      />
     </form>
   );
 };
