7f96a7af18d1a404a406c001ecb7cf244628fcf7
Jan Bańczerowski <jasiuu2605@gmail.com>
2024-08-26 17:11:39 +0200

    Adding google maps to apllication

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import "./Map.css";
 
 const Map = (props) => {
+  const mapRef = useRef();
 
+  const { center, zoom } = props;
+
+  useEffect(() => {
+    async function initMap() {
+      const { Map } = await window.google.maps.importLibrary("maps");
+      const { AdvancedMarkerElement } = await window.google.maps.importLibrary(
+        "marker"
+      );
+
+      const map = new Map(mapRef.current, {
+        zoom: zoom,
+        center: center,
+        mapId: "DEMO_MAP_ID",
+      });
+
+      new AdvancedMarkerElement({
+        map: map,
+        position: center,
+        title: "",
+      });
+    }
+
+    initMap();
+  }, [center, zoom]);
   return (
     <div
+      ref={mapRef}
       className={`map ${props.className}`}
       style={props.style}
       id="map"
