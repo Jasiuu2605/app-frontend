9e7525ff8a1fc507321b664eb087bd28d7fc63a1
Jan Bańczerowski <jasiuu2605@gmail.com>
2024-10-13 15:52:17 +0200

    Menaging deleting places

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -4,6 +4,9 @@ import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
 import Map from "../../shared/components/UIElements/Map";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import { AuthContext } from "../../shared/context/auth-context";
 
 import "./PlaceItem.css";
@@ -11,6 +14,7 @@ import "./PlaceItem.css";
 const PlaceItem = (props) => {
   const auth = useContext(AuthContext);
   const [showMap, setShowMap] = useState(false);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   const [showConfirmModal, setshowConfirmModal] = useState(false);
 
@@ -30,13 +34,20 @@ const PlaceItem = (props) => {
     setshowConfirmModal(false);
   };
 
-  const confirmDeleteHandler = () => {
+  const confirmDeleteHandler = async () => {
     setshowConfirmModal(false);
-    console.log("DELETING...");
+    try {
+      await sendRequest(
+        `http://localhost:5001/api/places/${props.id}`,
+        "DELETE"
+      );
+      props.onDelete(props.id);
+    } catch (error) {}
   };
 
   return (
     <>
+      <ErrorModal error={error} onClear={clearError} />
       <Modal
         show={showMap}
         onCancel={closeMapHandler}
@@ -70,7 +81,8 @@ const PlaceItem = (props) => {
         </p>
       </Modal>
       <li className="place-item">
-        <Card className="place-item_content">
+        <Card className="place-item__content">
+          {isLoading && <LoadingSpinner asOverlay />}
           <div className="place-item__image">
             <img src={props.image} alt={props.title} />
           </div>
