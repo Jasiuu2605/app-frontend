a25abd12a8779dacfa71544f0f077e294e6cb2ca
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-11-23 18:08:27 +0100

    feat: converting Users component with user fetching and loading states

---
+++ src/user/pages/Users.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+import UsersList from '../components/UsersList/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+
+// typ pojedynczego usera – zgodnie z backendem
+type User = {
+  id: string;
+  name: string;
+  image: string;
+  places: number;
+};
+
+function Users() {
+  const [loadedUsers, setLoadedUsers] = useState<User[] | undefined>();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const responseData = await sendRequest(
+          'http://localhost:5001/api/users'
+        );
+        setLoadedUsers(responseData.users as User[]);
+      } catch (error) {}
+    }
+
+    fetchUsers();
+  }, [sendRequest]);
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+
+      {isLoading && (
+        <div className='center'>
+          <LoadingSpinner />
+        </div>
+      )}
+
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </>
+  );
+}
+
+export default Users;
