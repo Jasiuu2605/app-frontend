39bb1868c4b5d1304e7d39658104847f3d3900dc
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-11-23 13:19:35 +0100

    feat: add ErrorModal component for error handling

--- src/shared/components/UIElements/ErrorModal.tsx
+++ src/shared/components/UIElements/ErrorModal.tsx
@@ -3,17 +3,22 @@ import React from 'react';
 import Modal from './Modal';
 import Button from '../FormElements/Button';
 
-const ErrorModal = props => {
+type ErrorModalProps = {
+  error?: string | null;
+  onClear: () => void;
+};
+
+function ErrorModal(props: ErrorModalProps) {
   return (
     <Modal
       onCancel={props.onClear}
-      header="An Error Occurred!"
+      header='An Error Occurred!'
       show={!!props.error}
       footer={<Button onClick={props.onClear}>Okay</Button>}
     >
       <p>{props.error}</p>
     </Modal>
   );
-};
+}
 
 export default ErrorModal;
